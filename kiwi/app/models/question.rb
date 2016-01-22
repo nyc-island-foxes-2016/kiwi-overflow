9e7a2d3db9ca59f78acced00a8aebbd9aaef77fb
Nicola Beuscher <email": null>
2016-01-22 16:27:22 -0500

    Add database constraints and model validations

--- kiwi/app/models/question.rb
+++ kiwi/app/models/question.rb
@@ -3,4 +3,7 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :comments
   has_many :votes
+
+  validates_presence_of :title, :content, :user_id
+
 end
--- kiwi/db/migrate/20160122211518_create_questions.rb
+++ kiwi/db/migrate/20160122211518_create_questions.rb
@@ -1,9 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string :title
-      t.text :content
-      t.integer :user_id
+      t.string :title, null: false
+      t.text :content, null: false
+      t.integer :user_id, null: false, index: true, foreign_key: true
       t.integer :best_answer_id
 
       t.timestamps null: false
