847b418bac8fdd994e266dd7064f40f69eabdf1f
Luis Fernando Plaz <luisplaz@hotmail.com>
2016-01-22 18:38:24 -0500

    Include controller methods for questions

---
+++ kiwi/app/controllers/questions_controller.rb
@@ -0,0 +1,45 @@
+class QuestionsController < ApplicationController
+
+  # before_filter
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to question_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @question = Question.find_by(id: params[:id])
+  end
+
+  def edit
+    @question = Question.find_by(id: params[:id])
+    if @question.update_attributes(question_params)
+      redirect_to question_path
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @question = Question.find_by(id: params[:id])
+    @question.destroy
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title,:content,:user_id,:best_answer_id)
+  end
+end
