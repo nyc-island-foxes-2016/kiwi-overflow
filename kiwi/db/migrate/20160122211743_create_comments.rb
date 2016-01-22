d2cb1309742779acc90d8417612f48a672fb0cad
Luis Fernando Plaz <luisplaz@hotmail.com>
2016-01-22 16:41:47 -0500

    Complete migrations information  comment and votes

--- kiwi/db/migrate/20160122211743_create_comments.rb
+++ kiwi/db/migrate/20160122211743_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :content, null: false, limit: 200
+      t.integer :commentable_id,  polymorphic: true, index: true null: false
+      t.string :commentable_type,  polymorphic: true, null:false
 
       t.timestamps null: false
     end
