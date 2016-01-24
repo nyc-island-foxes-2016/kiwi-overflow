eb0161c403f7d389dd927d693279f37bc03985ed
deweydell <nbeuscher@gmail.com>
2016-01-23 22:18:49 -0500

    Write basic specs for question model

---
+++ kiwi/spec/models/question_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Question do
+  let(:question) { FactoryGirl.create(:question) }
+
+  it "has a valid factory" do
+    expect(question).to be_valid
+  end
+
+  it "is invalid without a title" do
+    expect(FactoryGirl.build(:question, title: nil)).to_not be_valid
+  end
+
+  it "is invalid without a content" do
+    expect(FactoryGirl.build(:question, content: nil)).to_not be_valid
+  end
+
+  it "is invalid without a user_id" do
+    expect(FactoryGirl.build(:question, user_id: nil)).to_not be_valid
+  end
+
+end
