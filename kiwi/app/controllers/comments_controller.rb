16e3d005d11c57599881d2d66dbacbd3736c441a
Jeff George <webdevjeffus@gmail.com>
2016-01-23 19:46:13 -0500

    Remove binding.pry from comments#edit

--- kiwi/app/controllers/comments_controller.rb
+++ kiwi/app/controllers/comments_controller.rb
@@ -24,7 +24,6 @@ class CommentsController < ApplicationController
   def edit
     @comment = Comment.find_by(id: params[:id])
     @question = Question.find_by(id: @comment.commentable_id)
-    binding.pry
   end
 
   def update
