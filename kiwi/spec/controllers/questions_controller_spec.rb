0f452ff66959cf4f875c3ca195c34a617ecdb3d6
deweydell <nbeuscher@gmail.com>
2016-01-23 22:17:41 -0500

    Write get index spec for questions controller

--- kiwi/spec/controllers/questions_controller_spec.rb
+++ kiwi/spec/controllers/questions_controller_spec.rb
@@ -2,26 +2,14 @@ require 'rails_helper'
 
   describe QuestionsController do
 
-    it "#index" do
-      get :index
-      expect(assigns(:questions)).to eq Question.all
-    end
+    describe 'GET #index' do 
 
-    it "#new" do
-      get :new
-    end
-
-    context "#create" do
-
-      it "creates a question with valid params" do
-      post :create, question: {title: "valid", content: "this is content"}
-      expect(Question.find(assigns[:question].id)).to be true
+      it "populates an array of questions" do
+        question = FactoryGirl.create(:question)
+        get :index
+        expect(assigns(:questions)).to eq([question])
       end
 
-      it "doesn't create a question when params are invalid" do
-        post :create, question: {title: "invalid", content: ""}
-        expect(Question.find(assigns[:question].id)).to be false
-      end
     end
 
 
