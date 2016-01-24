17c1c05028b9a8de96a2e58164336a019bd0381e
deweydell <nbeuscher@gmail.com>
2016-01-23 21:21:00 -0500

    Add work on questions controller spec for create route

--- kiwi/spec/controllers/questions_controller_spec.rb
+++ kiwi/spec/controllers/questions_controller_spec.rb
@@ -7,7 +7,22 @@ require 'rails_helper'
       expect(assigns(:questions)).to eq Question.all
     end
 
+    it "#new" do
+      get :new
+    end
 
+    context "#create" do
+
+      it "creates a question with valid params" do
+      post :create, question: {title: "valid", content: "this is content"}
+      expect(Question.find(assigns[:question].id)).to be true
+      end
+
+      it "doesn't create a question when params are invalid" do
+        post :create, question: {title: "invalid", content: ""}
+        expect(Question.find(assigns[:question].id)).to be false
+      end
+    end
 
 
 end
